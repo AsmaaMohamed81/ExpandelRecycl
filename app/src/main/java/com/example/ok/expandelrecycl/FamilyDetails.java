950a507cdcffcdc9464a742b659456decb816cf3
emanzelekha <emanzelekha@gmail.com>
2017-12-30 11:06:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ok/expandelrecycl/FamilyDetails.java
@@ -0,0 +1,13 @@
+package com.example.ok.expandelrecycl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FamilyDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family_details);
+    }
+}
